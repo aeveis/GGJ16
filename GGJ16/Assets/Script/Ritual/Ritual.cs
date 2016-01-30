20940950706389cece3f3b879d3302c010e4c234
Henry Braun <hbraun87@gmail.com>
2016-01-30 10:37:58 -0500

    Civilian mechanics

--- GGJ16/Assets/Script/Ritual/Ritual.cs
+++ GGJ16/Assets/Script/Ritual/Ritual.cs
@@ -4,4 +4,6 @@ using System.Collections;
 public abstract class Ritual : MonoBehaviour
 {
     public abstract bool Check();
+    public abstract void Action(Transform p_Actor);
+    public abstract void Reset();
 }
