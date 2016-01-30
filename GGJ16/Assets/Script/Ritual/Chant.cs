42f93a9dec2d524c4fcfbacc28c0c1677daf65cc
aeveis <aeveis@gmail.com>
2016-01-30 15:44:46 -0500

    get rid of unused files

--- GGJ16/Assets/Script/Ritual/Chant.cs
+++ GGJ16/Assets/Script/Ritual/Chant.cs
@@ -4,6 +4,9 @@ using System;
 
 public class Chant : Ritual
 {
+	public string chantPattern ="-. -.";
+	public float startBufferTime = .2f;
+	public float chantSpeed = .5f;
 	public override bool Check(Transform p_Actor)
 	{
 		return !PlayerController.Instance.IsJumping();
