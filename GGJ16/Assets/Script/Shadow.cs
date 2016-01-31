7c80b6225f35112425d5756ede515a2013010d6c
aeveis <aeveis@gmail.com>
2016-01-31 14:00:58 -0500

    shadow for player

---
+++ GGJ16/Assets/Script/Shadow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shadow : MonoBehaviour {
+
+	public float y = 0;
+	private Vector3 pos;
+	private bool moveShadow = false;
+	void Awake () {
+		pos = new Vector3 ();
+		StartCoroutine (WaitforY ());
+	}
+
+	IEnumerator WaitforY(){
+		yield return new WaitForSeconds (.05f);
+		y = transform.position.y+.05f;
+		moveShadow = true;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (moveShadow) {
+			pos = transform.parent.position;
+			pos.y = y;
+			transform.position = pos;
+		}
+	
+	}
+}
