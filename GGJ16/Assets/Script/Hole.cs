5cb76ccbd9e135cda41dfd6f7065e02c85739170
SuperJujuAwesome <shawn@shawnpatton.com>
2016-01-30 22:20:37 -0500

    overwrote Connor

--- GGJ16/Assets/Script/Hole.cs
+++ GGJ16/Assets/Script/Hole.cs
@@ -9,6 +9,12 @@ public class Hole : MonoBehaviour
 	private Vector3 m_playerPosition;
 	public AudioClip fallSound;
 
+	void Start()
+	{
+		
+		//GetComponent<BoxCollider>().size = new Vector3( transform.localScale.x*0.05f + 0.75f, 1f, transform.localScale.z*0.05f + 0.75f);
+	}
+
     void OnTriggerEnter(Collider other)
     {
 		Debug.Log("hole death");
@@ -27,22 +33,25 @@ public class Hole : MonoBehaviour
 
 	public IEnumerator StartHoleDeath()
 	{
-		m_isDying = true;
-		m_playerPosition =  PlayerController.Instance.transform.position + (transform.position - PlayerController.Instance.transform.position).normalized * 0.5f;
-		yield return null;
-		SoundManager.instance.PlaySingle(fallSound);
-		float time = 0.0f;
-		while(time < 1.0f)
-		{
-			time += Time.deltaTime;
-			PlayerController.Instance.transform.localScale = Vector3.one * (1.0f - time);
-
+		yield return new WaitForSeconds(0.2f);
+		if (!PlayerController.Instance.IsJumping()){
+			m_isDying = true;
+			m_playerPosition =  PlayerController.Instance.transform.position + (transform.position - PlayerController.Instance.transform.position).normalized * 0.5f;
 			yield return null;
-		}
+			SoundManager.instance.PlaySingle(fallSound);
+			float time = 0.0f;
+			while(time < 1.0f)
+			{
+				time += Time.deltaTime;
+				PlayerController.Instance.transform.localScale = Vector3.one * (1.0f - time);
 
-		//reset
-		Application.LoadLevel(Application.loadedLevel);
-		//yield return new WaitForSeconds(0.1f);
-		m_isDying = false;
+				yield return null;
+			}
+
+			//reset
+			Application.LoadLevel(Application.loadedLevel);
+			//yield return new WaitForSeconds(0.1f);
+			m_isDying = false;
+		}
 	}
 }
